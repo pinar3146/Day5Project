4ea9b6be62a13eecab38834c482dabdee82dc5c1
pinar3146 <pinaryanar3146@hotmail.com>
2020-10-13 19:30:10 +0200

    added1

--- src/test/java/Feature1.java
+++ src/test/java/Feature1.java
@@ -1,5 +1,5 @@
 public class Feature1 {
     public static void main(String[] args) {
-        System.out.println("Feature 1 is created");
+        System.out.println("Feature 1 is created one time");
     }
 }
