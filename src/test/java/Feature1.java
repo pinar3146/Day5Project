1a70a3d7fb60af7936e4676b6aa440edc3d2cdbc
pinar3146 <pinaryanar3146@hotmail.com>
2020-10-13 19:39:43 +0200

    added some changes

--- src/test/java/Feature1.java
+++ src/test/java/Feature1.java
@@ -1,5 +1,5 @@
 public class Feature1 {
     public static void main(String[] args) {
-        System.out.println("Feature 1 is created one time.");
+        System.out.println("Feature 1 is created one time. and waiting");
     }
 }
