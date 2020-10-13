3b18bd228c785cce5a05a63524a7842df41fc3ae
pinar3146 <pinaryanar3146@hotmail.com>
2020-10-13 19:29:42 +0200

    added new class again and again

--- src/test/java/Feature2.java
+++ src/test/java/Feature2.java
@@ -1,6 +1,6 @@
 public class Feature2 {
     public static void main(String[] args) {
-        System.out.println("Feature2 is created again");
+        System.out.println("Feature2 is created again and again");
     }
 }
 
